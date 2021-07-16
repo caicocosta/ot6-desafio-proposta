c5f3f8de76841270d18e10988777a326ca2aa534
Caico Costa <sales.c.costa@gmail.com>
2021-07-16 11:35:47 -0300

    Criação de processo para associar cartao.
    Criado uma schedule para buscar propostas que estejam elegíveis, e depois acionar o serviço que gera os cartões, associar o cartão a proposta, e finalizá-la.

--- desafio-proposta/src/main/java/br/com/zupacademy/caico/proposta/criacaoproposta/PropostaRepository.java
+++ desafio-proposta/src/main/java/br/com/zupacademy/caico/proposta/criacaoproposta/PropostaRepository.java
@@ -1,11 +1,17 @@
 package br.com.zupacademy.caico.proposta.criacaoproposta;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import br.com.zupacademy.caico.proposta.criacaoproposta.analiserestricao.StatusProposta;
 
 @Repository
 public interface PropostaRepository extends JpaRepository<Propostas, Long>{
 
 	Propostas findByDocumento(String documento);
+
+	List<Propostas> findByStatus(StatusProposta elegivel);
 
 }
